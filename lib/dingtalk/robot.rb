a90709c3a6d1ed3bea6f047c85b4fe7bec320f40
Song Huang <songhuangcn@qq.com>
2018-09-16 17:05:41 +0800

    :tada: Init Gem

---
+++ lib/dingtalk/robot.rb
@@ -0,0 +1,7 @@
+require "dingtalk/robot/version"
+
+module Dingtalk
+  module Robot
+    # Your code goes here...
+  end
+end
---
+++ lib/dingtalk/robot/version.rb
@@ -0,0 +1,5 @@
+module Dingtalk
+  module Robot
+    VERSION = "0.1.0"
+  end
+end
